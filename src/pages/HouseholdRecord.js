243fe918f0b3dd9e9ceb932d2382d06f1aeaea00
katoriamu <118524077+katoriamu@users.noreply.github.com>
2022-11-26 21:47:54 +0800

    updated napo

--- src/pages/HouseholdRecord.js
+++ src/pages/HouseholdRecord.js
@@ -3,11 +3,64 @@ import "../styles/HouseholdRecord.css";
 import HouseholdRecordLinks from "../components/HouseholdRecordLinks";
 import { useSelector, useDispatch } from "react-redux";
 import { onChange } from "../features/HouseholdInputs";
+//drop down
+import { useTheme } from '@mui/material/styles';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+const names = [
+  'maburak',
+  'mabatu',
+  'madalumdum',
+  'makalbug',
+  'malubak',
+  'makantu',
+  'bugbugan street',
+  'sanpablo',
+  'US',
+  'Canada',
+];
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+const ITEM_HEIGHT = 30;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
 
 const HouseholdRecord = () => {
   const household = useSelector((state) => state.householdRecord.value);
   const dispatch = useDispatch();
   console.log(household)
+
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
   return (
     <div className="IndividualRecord">
       <div className="full__width IndividualRecord__column">
@@ -132,21 +185,27 @@ const HouseholdRecord = () => {
                     >
                       Barangay:
                     </label>
-                    <input
-                      className="IndividualRecord__input"
-                      type="text"
-                      name="barangay"
-                      placeholder="e.g (San Juan)"
-                      value={household.barangay}
-                      onChange={(e) =>
-                        dispatch(
-                          onChange({
-                            name: e.target.name,
-                            value: e.target.value,
-                          })
-                        )
-                      }
-                    />
+                    <FormControl sx={{ m: 1, width: 300 }}>
+                    <InputLabel id="demo-multiple-name-label">Barangay</InputLabel>
+                    <Select
+                       labelId="demo-multiple-name-label"
+                       id="demo-multiple-name"
+                       value={personName}
+                       onChange={handleChange}
+                       input={<OutlinedInput label="Barangay" />}
+                       MenuProps={MenuProps}
+                       >
+                     {names.map((name) => (
+                    <MenuItem
+                      key={name}
+                       value={name}
+                        style={getStyles(name, personName, theme)}
+                      >
+                         {name}
+                     </MenuItem>
+                     ))}
+                    </Select>
+                    </FormControl>
                   </div>
                   <div className="IndividualRecord__input__box__1">
                     <label
